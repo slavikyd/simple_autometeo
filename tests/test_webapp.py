16b4b0a80d67fbb3486645afe3c74c3485af89cd
slavikyd <slavikyd@gmail.com>
2024-06-13 16:15:06 +0300

    костыль на костыле

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -48,9 +48,9 @@ def test_create_func(json_create: dict, valid_json: tuple) -> None:
         valid_json (tuple): get sample
     """
     requests.post(
-        'http://localhost:5000/inner/meteodata/create', json=json_create, timeout=TOUT,
+        'http://localhost:5001/inner/meteodata/create', json=json_create, timeout=TOUT,
     )
-    response = requests.get('http://localhost:5000/inner/meteodata/get', timeout=TOUT).json()
+    response = requests.get('http://localhost:5001/inner/meteodata/get', timeout=TOUT).json()
     response = {key: value for key, value in response[0].items() if key != 'id'}
     valid_json[0] = {key: value for key, value in valid_json[0].items() if key != 'id'}
     assert response == valid_json[0]
@@ -63,10 +63,10 @@ def test_delete_func(status_code: int) -> None:
     Args:
         status_code (int): awaited response code
     """
-    response = requests.get('http://localhost:5000/inner/meteodata/get', timeout=TOUT).json()
+    response = requests.get('http://localhost:5001/inner/meteodata/get', timeout=TOUT).json()
     response_id = response[0]['id']
     response_code = requests.delete(
-        'http://127.0.0.1:5000/inner/meteodata/delete',
+        'http://127.0.0.1:5001/inner/meteodata/delete',
         json={'id': response_id},
         timeout=TOUT,
         ).status_code
